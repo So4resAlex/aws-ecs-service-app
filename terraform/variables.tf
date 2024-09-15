2ab14350c35408f55b6f17515efae537ec8a1498
Alex_Soares <alealvessoares28@gmail.com>
2024-09-14 23:39:28 -0300

    feat: add some resources

---
+++ terraform/variables.tf
@@ -0,0 +1,39 @@
+##### GENERAL CONFIGS #####
+variable "region" {}
+
+variable "cluster_name" {}
+
+variable "service_name" {}
+
+variable "service_port" {}
+
+variable "service_cpu" {}
+
+variable "service_memory" {}
+
+variable "service_healthcheck" {}
+
+variable "ssm_vpc_id" {}
+
+variable "ssm_listener" {}
+
+variable "service_launch_type" {}
+
+variable "service_task_count" {}
+
+variable "service_hosts" {}
+
+variable "ssm_private_subnet_1" {}
+
+variable "ssm_private_subnet_2" {}
+
+variable "ssm_private_subnet_3" {}
+
+variable "environment_variables" {
+  type = list(any)
+}
+
+variable "capabilities" {
+  type = list(any)
+}
+
