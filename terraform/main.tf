e84658bc705681d3a06ff6228f9afb22953e266a
Alex_Soares <alealvessoares28@gmail.com>
2024-10-03 08:32:35 -0300

    chore: fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
 module "servie" {
   source = "/home/alex/Desktop/repos-git/aws-ecs-service-module"
 
-  cluster_name   = var.cluster
-  service_name   = var.service_name
-  service_port   = var.service_port
-  service_cpu    = var.service_cpu
-  service_memory = var.service_memory
+  cluster_name     = var.cluster
+  service_name     = var.service_name
+  service_port     = var.service_port
+  service_cpu      = var.service_cpu
+  service_memory   = var.service_memory
   service_listener = data.aws_ssm_parameter.service_listener.value
-  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  vpc_id           = data.aws_ssm_parameter.vpc_id.value
   private_subnets = [
     data.aws_ssm_parameter.private_subnet_1.value,
     data.aws_ssm_parameter.private_subnet_2.value,
